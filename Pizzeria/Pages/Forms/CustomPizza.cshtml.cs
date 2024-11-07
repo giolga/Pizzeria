a28bba4ecafbf3bb24cad49aa22050ea9abc8ce1
giolga <giolga.kumi@gmail.com>
2024-11-07 14:36:47 +0400

    IActionResult OnPost And RedirectToPage

--- Pizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ Pizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,8 +8,24 @@ namespace Pizzeria.Pages.Forms
     {
         [BindProperty]
         public PizzasModel Pizza { get; set; }
+        public float PizzaPrice { get; set; }
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSouce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 2;
+            if (Pizza.Peperoni) PizzaPrice += 6;
+            if (Pizza.Mushroom) PizzaPrice += 6;
+            if (Pizza.Tuna) PizzaPrice += 3;
+            if (Pizza.Pineapple) PizzaPrice += 2;
+            if (Pizza.Ham) PizzaPrice += 3;
+            if (Pizza.Beef) PizzaPrice += 4;
+
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPrice });
         }
     }
 }
