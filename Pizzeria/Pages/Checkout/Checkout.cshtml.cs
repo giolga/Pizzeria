a1f3feb0834a37eaacc4ac80a7d291e970742842
giolga <giolga.kumi@gmail.com>
2024-11-07 14:58:51 +0400

    OnGet and displaying data from URL

--- Pizzeria/Pages/Checkout/Checkout.cshtml.cs
+++ Pizzeria/Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Pizzeria.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)] //Bind all these properties that will be accessible in the view
     public class CheckoutModel : PageModel
     {
+        public string PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
+
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+
+            if(string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
         }
     }
 }
