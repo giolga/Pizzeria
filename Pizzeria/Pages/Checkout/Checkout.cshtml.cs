7b62a966d433822d140cad7be5e77be8249839f7
giolga <giolga.kumi@gmail.com>
2024-11-08 14:35:03 +0400

    Save Pizza Orders

--- Pizzeria/Pages/Checkout/Checkout.cshtml.cs
+++ Pizzeria/Pages/Checkout/Checkout.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pizzeria.Data;
+using Pizzeria.Models;
 
 namespace Pizzeria.Pages.Checkout
 {
@@ -10,6 +12,12 @@ namespace Pizzeria.Pages.Checkout
         public float PizzaPrice { get; set; }
         public string ImageTitle { get; set; }
 
+        private readonly AppDbContext _context;
+        public CheckoutModel(AppDbContext context)
+        {
+            this._context = context;
+        }
+
         public void OnGet()
         {
             if (string.IsNullOrWhiteSpace(PizzaName))
@@ -17,10 +25,17 @@ namespace Pizzeria.Pages.Checkout
                 PizzaName = "Custom";
             }
 
-            if(string.IsNullOrWhiteSpace(ImageTitle))
+            if (string.IsNullOrWhiteSpace(ImageTitle))
             {
                 ImageTitle = "Create";
             }
+
+            PizzaOrder pizzaOrder = new PizzaOrder();
+            pizzaOrder.PizzaName = PizzaName;
+            pizzaOrder.BasePrice = PizzaPrice;
+
+            _context.PizzaOrders.Add(pizzaOrder);
+            _context.SaveChanges();
         }
     }
 }
